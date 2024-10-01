7061827c872811e04be729c10d62264d352c1d40
ryo izuta <r.izuta@mplpl.com>
2024-10-01 14:54:06 +0900

    ⚡️ remove works

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export", // 静的エクスポートを有効にする
-  // 他のオプションをここに追加できます
+module.exports = {
   images: {
     unoptimized: true, // 画像最適化を行わない
   },
 };
-
-module.exports = nextConfig;
