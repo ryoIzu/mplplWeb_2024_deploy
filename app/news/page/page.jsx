8f1d2890aa70b56fac42ba9b3e67b6dda7827e18
ryo izuta <r.izuta@mplpl.com>
2024-10-01 12:53:24 +0900

    suspense修正

--- app/news/page/page.jsx
+++ app/news/page/page.jsx
@@ -49,235 +49,233 @@ export default function News() {
   }, [slicedNews]);
 
   return (
-    <>
-      <Suspense fallback={<div>Loading...</div>}>
-        <div style={{ height: "50px" }}></div>
-        {isSort ? (
-          <>
-            {slicedNews.map((a_news, index) => {
-              if (a_news.isShow) {
-                return (
-                  <div key={a_news.date} className="div_content">
-                    <div className="div_date">
-                      <p>
-                        {a_news.date.slice(0, 4)}.{a_news.date.slice(4, 6)}.
-                        {a_news.date.slice(6, 8)}
-                      </p>
-                    </div>
-                    <div className="div_info">
-                      <a
-                        id="title"
-                        href={`./${a_news.date}?index=${(pageNum - 1) * showNewsNum + index}`}
-                      >
-                        {a_news.title}
-                      </a>
-                      <br />
-                      <a
-                        href={`./${a_news.date}?index=${(pageNum - 1) * showNewsNum + index}`}
-                      >
-                        more info...
-                      </a>
-                    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div style={{ height: "50px" }}></div>
+      {isSort ? (
+        <>
+          {slicedNews.map((a_news, index) => {
+            if (a_news.isShow) {
+              return (
+                <div key={a_news.date} className="div_content">
+                  <div className="div_date">
+                    <p>
+                      {a_news.date.slice(0, 4)}.{a_news.date.slice(4, 6)}.
+                      {a_news.date.slice(6, 8)}
+                    </p>
                   </div>
-                );
-              }
-              return null;
-            })}
-            <Pagination className="custom-pagination">
-              <Pagination.First
-                className="custom-pagination-item"
-                href={`./page?pageNum=1`}
-              />
-              {pageNum === 1 ? (
-                <>
-                  <Pagination.Item
-                    active={true}
-                    className="custom-pagination-item"
-                  >
-                    {pageNum}
-                  </Pagination.Item>
-                  <Pagination.Item
-                    className="custom-pagination-item"
-                    href={`./page?pageNum=${pageNum + 1}`}
-                  >
-                    {pageNum + 1}
-                  </Pagination.Item>
-                  <Pagination.Item
-                    className="custom-pagination-item"
-                    disabled={true}
-                  >
-                    ...
-                  </Pagination.Item>
-                  <Pagination.Next
-                    className="custom-pagination-item"
-                    href={`./page?pageNum=${pageNum + 1}`}
-                  >
-                    Next
-                  </Pagination.Next>
-                </>
-              ) : pageNum === 2 ? (
-                <>
-                  <Pagination.Prev
-                    className="custom-pagination-item"
-                    href={`./page?pageNum=${pageNum - 1}`}
-                  >
-                    Prev
-                  </Pagination.Prev>
-                  <Pagination.Item
-                    className="custom-pagination-item"
-                    href={`./page?pageNum=${pageNum - 1}`}
-                  >
-                    {pageNum - 1}
-                  </Pagination.Item>
-                  <Pagination.Item
-                    className="custom-pagination-item"
-                    href={`./page?pageNum=${pageNum}`}
-                    active={true}
-                    disabled={true}
-                  >
-                    {pageNum}
-                  </Pagination.Item>
-                  <Pagination.Item
-                    className="custom-pagination-item"
-                    href={`./page?pageNum=${pageNum + 1}`}
-                  >
-                    {pageNum + 1}
-                  </Pagination.Item>
-                  <Pagination.Item
-                    className="custom-pagination-item"
-                    disabled={true}
-                  >
-                    ...
-                  </Pagination.Item>
-                  <Pagination.Next
-                    className="custom-pagination-item"
-                    href={`./page?pageNum=${pageNum + 1}`}
-                  >
-                    Next
-                  </Pagination.Next>
-                </>
-              ) : pageNum > 2 && pageNum < lastPage - 1 ? (
-                <>
-                  <Pagination.Prev
-                    className="custom-pagination-item"
-                    href={`./page?pageNum=${pageNum - 1}`}
-                  >
-                    Prev
-                  </Pagination.Prev>
-                  <Pagination.Item
-                    className="custom-pagination-item"
-                    disabled={true}
-                  >
-                    ...
-                  </Pagination.Item>
-                  <Pagination.Item
-                    className="custom-pagination-item"
-                    href={`./page?pageNum=${pageNum - 1}`}
-                  >
-                    {pageNum - 1}
-                  </Pagination.Item>
-                  <Pagination.Item
-                    className="custom-pagination-item"
-                    active={true}
-                  >
-                    {pageNum}
-                  </Pagination.Item>
-                  <Pagination.Item
-                    className="custom-pagination-item"
-                    href={`./page?pageNum=${pageNum + 1}`}
-                  >
-                    {pageNum + 1}
-                  </Pagination.Item>
-                  <Pagination.Item className="custom-pagination-item">
-                    ...
-                  </Pagination.Item>
-                  <Pagination.Next
-                    className="custom-pagination-item"
-                    href={`./page?pageNum=${pageNum + 1}`}
-                  >
-                    Next
-                  </Pagination.Next>
-                </>
-              ) : pageNum === lastPage - 1 ? (
-                <>
-                  <Pagination.Prev
-                    className="custom-pagination-item"
-                    href={`./page?pageNum=${pageNum - 1}`}
-                  >
-                    Prev
-                  </Pagination.Prev>
-                  <Pagination.Item
-                    className="custom-pagination-item"
-                    disabled={true}
-                  >
-                    ...
-                  </Pagination.Item>
-                  <Pagination.Item
-                    className="custom-pagination-item"
-                    href={`./page?pageNum=${pageNum - 1}`}
-                  >
-                    {pageNum - 1}
-                  </Pagination.Item>
-                  <Pagination.Item
-                    className="custom-pagination-item"
-                    active={true}
-                  >
-                    {pageNum}
-                  </Pagination.Item>
-                  <Pagination.Item
-                    className="custom-pagination-item"
-                    href={`./page?pageNum=${pageNum + 1}`}
-                  >
-                    {pageNum + 1}
-                  </Pagination.Item>
-                  <Pagination.Next
-                    className="custom-pagination-item"
-                    href={`./page?pageNum=${pageNum + 1}`}
-                  >
-                    Next
-                  </Pagination.Next>
-                </>
-              ) : pageNum === lastPage ? (
-                <>
-                  <Pagination.Prev
-                    className="custom-pagination-item"
-                    href={`./page?pageNum=${pageNum - 1}`}
-                  >
-                    Prev
-                  </Pagination.Prev>
-                  <Pagination.Item
-                    className="custom-pagination-item"
-                    disabled={true}
-                  >
-                    ...
-                  </Pagination.Item>
-                  <Pagination.Item
-                    className="custom-pagination-item"
-                    href={`./page?pageNum=${pageNum - 1}`}
-                  >
-                    {pageNum - 1}
-                  </Pagination.Item>
-                  <Pagination.Item
-                    className="custom-pagination-item"
-                    active={true}
-                  >
-                    {pageNum}
-                  </Pagination.Item>
-                </>
-              ) : (
-                <></>
-              )}
+                  <div className="div_info">
+                    <a
+                      id="title"
+                      href={`./${a_news.date}?index=${(pageNum - 1) * showNewsNum + index}`}
+                    >
+                      {a_news.title}
+                    </a>
+                    <br />
+                    <a
+                      href={`./${a_news.date}?index=${(pageNum - 1) * showNewsNum + index}`}
+                    >
+                      more info...
+                    </a>
+                  </div>
+                </div>
+              );
+            }
+            return null;
+          })}
+          <Pagination className="custom-pagination">
+            <Pagination.First
+              className="custom-pagination-item"
+              href={`./page?pageNum=1`}
+            />
+            {pageNum === 1 ? (
+              <>
+                <Pagination.Item
+                  active={true}
+                  className="custom-pagination-item"
+                >
+                  {pageNum}
+                </Pagination.Item>
+                <Pagination.Item
+                  className="custom-pagination-item"
+                  href={`./page?pageNum=${pageNum + 1}`}
+                >
+                  {pageNum + 1}
+                </Pagination.Item>
+                <Pagination.Item
+                  className="custom-pagination-item"
+                  disabled={true}
+                >
+                  ...
+                </Pagination.Item>
+                <Pagination.Next
+                  className="custom-pagination-item"
+                  href={`./page?pageNum=${pageNum + 1}`}
+                >
+                  Next
+                </Pagination.Next>
+              </>
+            ) : pageNum === 2 ? (
+              <>
+                <Pagination.Prev
+                  className="custom-pagination-item"
+                  href={`./page?pageNum=${pageNum - 1}`}
+                >
+                  Prev
+                </Pagination.Prev>
+                <Pagination.Item
+                  className="custom-pagination-item"
+                  href={`./page?pageNum=${pageNum - 1}`}
+                >
+                  {pageNum - 1}
+                </Pagination.Item>
+                <Pagination.Item
+                  className="custom-pagination-item"
+                  href={`./page?pageNum=${pageNum}`}
+                  active={true}
+                  disabled={true}
+                >
+                  {pageNum}
+                </Pagination.Item>
+                <Pagination.Item
+                  className="custom-pagination-item"
+                  href={`./page?pageNum=${pageNum + 1}`}
+                >
+                  {pageNum + 1}
+                </Pagination.Item>
+                <Pagination.Item
+                  className="custom-pagination-item"
+                  disabled={true}
+                >
+                  ...
+                </Pagination.Item>
+                <Pagination.Next
+                  className="custom-pagination-item"
+                  href={`./page?pageNum=${pageNum + 1}`}
+                >
+                  Next
+                </Pagination.Next>
+              </>
+            ) : pageNum > 2 && pageNum < lastPage - 1 ? (
+              <>
+                <Pagination.Prev
+                  className="custom-pagination-item"
+                  href={`./page?pageNum=${pageNum - 1}`}
+                >
+                  Prev
+                </Pagination.Prev>
+                <Pagination.Item
+                  className="custom-pagination-item"
+                  disabled={true}
+                >
+                  ...
+                </Pagination.Item>
+                <Pagination.Item
+                  className="custom-pagination-item"
+                  href={`./page?pageNum=${pageNum - 1}`}
+                >
+                  {pageNum - 1}
+                </Pagination.Item>
+                <Pagination.Item
+                  className="custom-pagination-item"
+                  active={true}
+                >
+                  {pageNum}
+                </Pagination.Item>
+                <Pagination.Item
+                  className="custom-pagination-item"
+                  href={`./page?pageNum=${pageNum + 1}`}
+                >
+                  {pageNum + 1}
+                </Pagination.Item>
+                <Pagination.Item className="custom-pagination-item">
+                  ...
+                </Pagination.Item>
+                <Pagination.Next
+                  className="custom-pagination-item"
+                  href={`./page?pageNum=${pageNum + 1}`}
+                >
+                  Next
+                </Pagination.Next>
+              </>
+            ) : pageNum === lastPage - 1 ? (
+              <>
+                <Pagination.Prev
+                  className="custom-pagination-item"
+                  href={`./page?pageNum=${pageNum - 1}`}
+                >
+                  Prev
+                </Pagination.Prev>
+                <Pagination.Item
+                  className="custom-pagination-item"
+                  disabled={true}
+                >
+                  ...
+                </Pagination.Item>
+                <Pagination.Item
+                  className="custom-pagination-item"
+                  href={`./page?pageNum=${pageNum - 1}`}
+                >
+                  {pageNum - 1}
+                </Pagination.Item>
+                <Pagination.Item
+                  className="custom-pagination-item"
+                  active={true}
+                >
+                  {pageNum}
+                </Pagination.Item>
+                <Pagination.Item
+                  className="custom-pagination-item"
+                  href={`./page?pageNum=${pageNum + 1}`}
+                >
+                  {pageNum + 1}
+                </Pagination.Item>
+                <Pagination.Next
+                  className="custom-pagination-item"
+                  href={`./page?pageNum=${pageNum + 1}`}
+                >
+                  Next
+                </Pagination.Next>
+              </>
+            ) : pageNum === lastPage ? (
+              <>
+                <Pagination.Prev
+                  className="custom-pagination-item"
+                  href={`./page?pageNum=${pageNum - 1}`}
+                >
+                  Prev
+                </Pagination.Prev>
+                <Pagination.Item
+                  className="custom-pagination-item"
+                  disabled={true}
+                >
+                  ...
+                </Pagination.Item>
+                <Pagination.Item
+                  className="custom-pagination-item"
+                  href={`./page?pageNum=${pageNum - 1}`}
+                >
+                  {pageNum - 1}
+                </Pagination.Item>
+                <Pagination.Item
+                  className="custom-pagination-item"
+                  active={true}
+                >
+                  {pageNum}
+                </Pagination.Item>
+              </>
+            ) : (
+              <></>
+            )}
 
-              <Pagination.Last
-                className="custom-pagination-item"
-                href={`./page?pageNum=${Math.ceil(newsInfo.amount / showNewsNum, 10)}`}
-              />
-            </Pagination>
-          </>
-        ) : (
-          <></>
-        )}
-      </Suspense>
-    </>
+            <Pagination.Last
+              className="custom-pagination-item"
+              href={`./page?pageNum=${Math.ceil(newsInfo.amount / showNewsNum, 10)}`}
+            />
+          </Pagination>
+        </>
+      ) : (
+        <></>
+      )}
+    </Suspense>
   );
 }
